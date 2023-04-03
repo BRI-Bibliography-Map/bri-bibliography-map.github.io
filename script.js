8be8d5fa924aedb8785c98baa1d5a218b80eb3bf
Ryan Cobourn <rjcobourn@gmail.com>
2023-04-02 20:40:22 -0700

    Added country level popups

--- script.js
+++ script.js
@@ -10,11 +10,25 @@ const slugToName = [
 ];
 
 let articles;
+let countryData = {};
 
 d3.csv("data.csv")
   .then(function (data) {
     articles = data;
     console.log(articles);
+  })
+  .catch(function (error) {
+    // handle error
+  });
+
+d3.csv("countryData.csv")
+  .then(function (data) {
+    //convert array of objects to object of objects with country as key and the rest of the data as value
+    data.forEach((d) => {
+      countryData[d.Country] = d;
+      // remove the country key from the value
+      delete countryData[d.Country].Country;
+    });
   })
   .catch(function (error) {
     // handle error
@@ -28,30 +42,80 @@ const map = new mapboxgl.Map({
   style: "mapbox://styles/prince2351/claexaqnl000k14n7sw0j9mzj",
 });
 
-map.on("click", (event) => {
+// once the map loads, add the data source
+// this is using the Mapbox API
+
+function handleRegion(event) {
   // If the user clicked on one of your markers, get its information.
   const features = map.queryRenderedFeatures(event.point, {
-    layers: slugToName, // replace with your layer name
+    layers: slugToName,
   });
 
-  if (!features.length) {
-    return;
-  }
   const feature = features[0];
-
-  console.log(feature.layer.id);
-
-  console.log(
-    articles.filter((article) => article.Region === feature.layer.id)
-  );
 
   const popup = new mapboxgl.Popup({ offset: [0, -15] })
     .setLngLat(event.lngLat)
     .setHTML(
-      `<h3>Study Information</h3>${articles
+      `<h3>${feature.layer.id} Study Information</h3>${articles
         .filter((article) => feature.layer.id === article.Region)
         .map((article) => `<p>${article.Study_Information}</p>`)
         .join("")}`
     )
     .addTo(map);
+}
+
+function handleCountry(event, country) {
+  // If the user clicked on one of your markers, get its information.
+  const features = map.queryRenderedFeatures(event.point);
+
+  // create a new object with the country data only with values greater than 0
+  const localData = Object.keys(countryData[country]).reduce((acc, key) => {
+    if (countryData[country][key] > 0) {
+      acc[key] = countryData[country][key];
+    }
+    return acc;
+  }, {});
+
+  // sort by value
+  const sortedData = Object.keys(localData)
+    .sort((a, b) => localData[b] - localData[a])
+    .reduce((acc, key) => {
+      acc[key] = localData[key];
+      return acc;
+    }, {});
+
+  const countryDataHTML = Object.keys(sortedData).map(
+    (key) => `${sortedData[key]} - ${key}`
+  );
+
+  console.log(countryDataHTML);
+
+  // display the popup with data from the countryData object for the country
+  const popup = new mapboxgl.Popup({ offset: [0, -15] })
+    .setLngLat(event.lngLat)
+    .setHTML(
+      `<h3>${country} Study Information</h3>${countryDataHTML
+        .map((data) => `<p>${data}</p>`)
+        .join("")}`
+    )
+    .addTo(map);
+}
+
+map.on("click", (event) => {
+  // log all features under the mouse
+  console.log(map.queryRenderedFeatures(event.point)[0].properties);
+
+  if (!map.queryRenderedFeatures(event.point).length) {
+    return;
+  }
+
+  // if it has the continent property, then it is a continent / region
+  if (map.queryRenderedFeatures(event.point)[0].properties.Continent) {
+    handleRegion(event);
+  } else if (map.queryRenderedFeatures(event.point)[0].properties.ADMIN) {
+    handleCountry(
+      event,
+      map.queryRenderedFeatures(event.point)[0].properties.ADMIN
+    );
+  }
 });
---
+++ script.py
@@ -0,0 +1,66 @@
+# import csv module
+import csv
+
+# country dictionary format
+# { "country": { "Political Science and International Relations": numOccurances } }
+
+subjects = [
+ "Political Science and International Relations",
+ "Sociology and Political Science",
+ "Development",
+ "Economics, Econometrics and Finance",
+ "Business, Management and Accounting",
+ "Environmental Science",
+ "Transportation",
+ "Social Sciences (Misc.)",
+ "Agricultural and Biological Sciences",
+ "Arts and Humanities",
+ "Computer Science",
+ "Decision Sciences",
+ "Earth and Planetary Sciences",
+ "Education",
+ "Energy",
+ "Law",
+ "Mathematics",
+ "Medicine",
+ "Multidisciplinary",
+ "Safety Research",
+ "Urban Studies",
+ "Cultural Sciences",
+]
+
+nations = {}
+
+# open the csv file in read mode
+with open('stupiddumbdata.csv', 'r') as read_obj:
+    # pass the file object to reader() to get the reader object
+    csv_reader = csv.reader(read_obj)
+    next(csv_reader)
+    # Iterate over each row in the csv using reader object
+    for row in csv_reader:
+        # split row by " / " in the first column
+        # and store the result in a list with rest of the columns
+        list = row[0].split(" / ")
+        # for each element in list, make a row containing the first element and the rest of the columns
+        for nation in list:
+            # if the country is not in the dictionary, add it
+            if nation not in nations:
+                nations[nation] = { }
+                # add subjects to the dictionary with initial value of 0
+                for subject in subjects:
+                    nations[nation][subject] = 0
+            # for each subject in the row, add 1 to the value
+            for subject in subjects:
+                if row[subjects.index(subject) + 1] == "1":
+                    nations[nation][subject] += 1
+        # print out the nations dictionary
+
+# output the dictionary to a csv file
+with open('output.csv', 'w', newline='', encoding='utf-8') as f:
+    # create the csv writer
+    writer = csv.writer(f)
+    # write the header row
+    writer.writerow(["Country"] + subjects)
+    # write the data without newlines
+    for nation in nations:
+        writer.writerow([nation] + [nations[nation][subject] for subject in subjects])
