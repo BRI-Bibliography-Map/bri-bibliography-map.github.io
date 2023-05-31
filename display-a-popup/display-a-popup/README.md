40ae0e9c725bd50cc3e3e576d21f1b7e52da485c
Prince12o <125172175+Prince12o@users.noreply.github.com>
2023-05-31 16:33:52 -0700

    Add files via upload

---
+++ display-a-popup/display-a-popup/README.md
@@ -0,0 +1,7 @@
+# Display a popup
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/bpo12/pen/rNqEdaa](https://codepen.io/bpo12/pen/rNqEdaa).
+
+Add a popup to the map.
+
+See the example: [https://docs.mapbox.com//mapbox-gl-js/example/popup/](https://docs.mapbox.com//mapbox-gl-js/example/popup/)
---
+++ display-a-popup/display-a-popup/src/script.js
@@ -0,0 +1,17 @@
+
+	// TO MAKE THE MAP APPEAR YOU MUST
+	// ADD YOUR ACCESS TOKEN FROM
+	// https://account.mapbox.com
+	mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
+    const map = new mapboxgl.Map({
+        container: 'map',
+        // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [-96, 37.8],
+        zoom: 2
+    });
+
+    const popup = new mapboxgl.Popup({ closeOnClick: false })
+        .setLngLat([-100, 50])
+        .setHTML('<h1>Welcome to the University of Virginia map of scholarly literature about the Belt and Road Initiative. Scroll to zoom for the country layer, and click on any location to see information.</h1>')
+        .addTo(map);
